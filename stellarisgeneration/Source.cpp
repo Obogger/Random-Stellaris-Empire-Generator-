413acf6cb0de046a1fae8097a2925fa755625d16
Obogg <kevdogfara@outlook.com>
2023-04-04 17:12:08 +0200

    goffy

--- stellarisgeneration/Source.cpp
+++ stellarisgeneration/Source.cpp
@@ -29,7 +29,7 @@ int gameTypeTraits[500] = { 300, 60 };
 string prompt[] = { "Apperance Race: ", "Apperance Type: ", "Origin: ", "Planet Class: ", "Species Quality: ",
 "Traits: ", "Government Type: ", "Government Ethics: " };
 
-string qualityType[] = { "Failure", "Incapable", "Inferior", "Avrage", "Avrage", "Avrage", "Avrage", "Avrage", "Superior", "Gifted", "Perfect"
+string qualityType[] = { "Failure", "Incapable", "Inferior", "Avrage", "Avrage", "Avrage", "Avrage", "Avrage", "Superior", "Gifted", "Perfect",
 "Scrap", "Scrap", "Scrap" , "Avrage", "Avrage", "Avrage", "Avrage", "Avrage", "Excellent", "Excellent", "Excellent" };
 
 
@@ -46,7 +46,7 @@ string gameEthics[] = { "Fanatic Libertarian", "Fanatic Militarist", "Fanatic Xe
 "Libertarian", "Militarist", "Xenophobe", "Competitive", "Elitist", "Materialist", "Anthropocentric", "Authoritarian", "Pacifist", "Xenophile", "Cooperative",
 "Pluralist", "Spiritualist", "Ecocentric" };
 
-string originRestrictions[] = { ":", "05+19+G-:", "G-:", "M-:", "M-:", ":", ":", "G-", "G-2-:", ":", "H+:", "G-:" };
+string originRestrictions[] = { ":", "G+050+:", "G-:", "M-:", "M-:", ":", ":", "G-:", "G-2-:", ":", "H+:", "G-:", "L+:", "M+:", "G-020-:", "G-020-", ":", "G-:", "M-090-00-", "G-:" };
 
 int main()
 {
@@ -66,52 +66,17 @@ void randomCalculator(int type)
 	string traitQuality;
 
 	game[0] = rand() % gameTypeApperance[gameFrom];
-	cout << prompt[0] << gameTypeApperanceName[game[0]] << endl;
 	game[1] = rand() % gameTypeApperance[gameFrom + game[0] + 1];
-	cout << prompt[1] << game[1] << endl;
 	game[2] = rand() % gameTypeOrigin[type - 1];
-	game[2] = 1;
+	game[3] = rand() % gameTypeWorld[type - 1] + 1;
+	game[2] = rand() % 20;
 	int temp = 0;
 	int restrictions[] = { 99,99,99,99,99,99,99 };
 	int i = 0;
-	for(int c = 0; originRestrictions[game[2]][c] != ':' ; c++)
-	{
-		if (originRestrictions[game[2]][c] != '+'&& originRestrictions[game[2]][c] != '-')
-		{
-			if (int(originRestrictions[game[2]][c] - 48) / 10 == 0)
-			{
-				temp = temp * 10 + int(originRestrictions[game[2]][c] - 48);
-
-
-			}
-			else
-			{
-
-			}
-		}
-		else
-		{
-			if (originRestrictions[game[2]][c] == '+')
-			{
-				restrictions[i];
-			}
-			else if(originRestrictions[game[2]][c] == '-')
-			{
-
-				restrictions[i] = temp;
-			}
-			temp = 0;
-			i++;
-		}
-
-	}
-	cout << prompt[2] << ((game[2] - 1) / 4) + 1 << "/" << ((gameTypeOrigin[0]) / 4) - ((game[2] - 1) / 4) + 1
-		<< ":" << ((game[2] - 1) - ((game[2] - 1) / 4) * 4) + 1 << endl;
-
-	game[3] = rand() % gameTypeWorld[type - 1] + 1;
-	cout << prompt[3] << game[3] << endl;
-
-	if (game[0] == 2)
+	bool exception = false;
+	int fnatic = 0;
+	int normal = 0;
+	if (game[0] == 1)
 	{
 		traitQuality = qualityType[rand() % 11 + 11];
 	}
@@ -119,45 +84,137 @@ void randomCalculator(int type)
 	{
 		traitQuality = qualityType[rand() % 11];
 	}
-	cout << prompt[4] << traitQuality << endl;
-	cout << prompt[5] << endl;
-	for (int t = 5; t < 11; t++)
-	{
-		game[t] = rand() % 300 + 1;
-		cout << game[t] << endl;
-
-	}
-
-	cout << endl << endl;
 	bool isGestalt = NULL;
-	cout << prompt[6];
-	if (game[0] == 2)
+	if (game[0] == 1)
 	{
-		cout << "Machine Intelligence";
+		game[12] = 0;
 		isGestalt = true;
 	}
 	else if (rand() % 100 + 1 <= 10)
 	{
-		cout << "Hivemind";
+		game[12] = 1;
 		isGestalt = true;
 	}
 	else
 	{
-		cout << "Normal";
+		game[12] = 2;
 		isGestalt = false;
 	}
-	cout << endl << prompt[7] << endl;
+	char excep = 'q';
+	int turn = 0;
+	for (int c = 0; originRestrictions[game[2]][c] != ':'; c++)
+	{
+		if (originRestrictions[game[2]][c] != '+' && originRestrictions[game[2]][c] != '-')
+		{
+			if (int(originRestrictions[game[2]][c] - 48) < 10)
+			{
+				temp = temp * 10 + int(originRestrictions[game[2]][c] - 48);
+				turn++;
+
+			}
+			else
+			{
+				excep = originRestrictions[game[2]][c];
+				exception = true;
+			}
+		}
+		else
+		{
+			if (originRestrictions[game[2]][c] == '+')
+			{
+
+				if (exception == true)
+				{
+					if (excep == 'G')
+					{
+						if (isGestalt == false)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					else if (excep == 'M')
+					{
+						if (game[0] != 1)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					else if (excep == 'H')
+					{
+						if (game[12] != 1)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					exception = false;
+				}
+				else if (turn)
+				{
+
+				}
+				turn = 0;
+			}
+			else if (originRestrictions[game[2]][c] == '-')
+			{
+				if (exception == true)
+				{
+					if (excep == 'G')
+					{
+						if (isGestalt == true)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					else if (excep == 'M')
+					{
+						if (game[0] == 1)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					else if (excep == 'H')
+					{
+						if (game[12] == 1)
+						{
+							c = -1;
+							game[2] = rand() % gameTypeOrigin[type - 1];
+							game[2] = rand() % 20;
+						}
+					}
+					exception = false;
+					turn = 0;
+				}
+				//restrictions[i] = temp;
+			}
+			temp = 0;
+			exception = false;
+			i++;
+		}
+
+	}
+	int current[20] = { 99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99, };
+	int c = 0;
+	i = 0;
 	if (isGestalt == true)
 	{
-		int current[6] = { 99,99,99,99,99,99 };
-		cout << gameGestaltEthics[rand() % 6] << endl;
+		game[13] = rand() % 6;
 		int success = rand() % 3 - 3;
-		int i = 0;
+		i = 0;
 		int avaiable = 0;
 		while (success < 0)
 		{
 			avaiable = rand() % 6 + 6;
-			int c = 0;
+			c = 0;
 			for (c = 0; c < 7; c++)
 			{
 				if (avaiable != current[c])
@@ -185,27 +242,21 @@ void randomCalculator(int type)
 				}
 			}
 		}
-		for (int c = 0; c < i; c++)
-		{
-			cout << gameGestaltEthics[current[c]] << endl;
-		}
-
 	}
-	else 
+	else
 	{
-		int fnatic = rand() % 3;
-		int normal = rand() % (6 - fnatic * 2);
+		fnatic = rand() % (3 - fnatic);
+		normal = rand() % (6 - fnatic * 2);
 		int d = 3;
 		while (d > fnatic * 2 + normal)
 		{
 			fnatic = rand() % 3;
 			normal = rand() % (6 - fnatic * 2);
 		}
-		int current[20] = { 99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99, };
 		int success = fnatic;
-		int i = 0;
+		i = 0;
 		int avaiable = 0;
-		int c = 0;
+		c = 0;
 		int round = 0;
 		while ((success > 0) || (fnatic == 0))
 		{
@@ -221,7 +272,7 @@ void randomCalculator(int type)
 			}
 			for (c; c < 21; c++)
 			{
-				if (avaiable != current[c] || avaiable != restrictions[c])
+				if (avaiable != current[c] && avaiable != restrictions[c])
 				{
 					if (c == 20)
 					{
@@ -248,7 +299,7 @@ void randomCalculator(int type)
 					c = 100;
 				}
 			}
-			if (success== 0)
+			if (success == 0)
 			{
 				success = normal;
 				normal = -99;
@@ -256,11 +307,42 @@ void randomCalculator(int type)
 				fnatic = 2;
 			}
 		}
+	}
+	cout << prompt[0] << gameTypeApperanceName[game[0]] << endl;
+	cout << prompt[1] << game[1] << endl;
+	cout << prompt[2] << ((game[2]) / 4) + 1 << "/" << ((gameTypeOrigin[0]) / 4) - ((game[2]) / 4) + 1
+		<< ":" << ((game[2]) - ((game[2] - 1) / 4) * 4) + 1 << endl;
+	cout << prompt[3] << game[3] << endl;
+	cout << prompt[4] << traitQuality << endl;
+	cout << prompt[5] << endl;
+	cout << prompt[6];
+	if (game[12] == 0)
+	{
+		cout << "Machine Intelligence";
+	}
+	else if (game[12] == 1)
+	{
+		cout << "Hivemind";
+	}
+	else
+	{
+		cout << "Normal";
+	}
+	cout << endl << prompt[7] << endl;
+	if (isGestalt == true)
+	{
+		cout << gameGestaltEthics[game[13]] << endl;
+		for (int c = 0; c < i; c++)
+		{
+			cout << gameGestaltEthics[current[c]] << endl;
+		}
+	}
+	else
+	{
 		for (int c = 0; c < i; c++)
 		{
 			cout << gameEthics[current[c]] << endl;
 		}
-
 	}
 	cout << "\n\n\nREDO? \n";
 	system("pause");
